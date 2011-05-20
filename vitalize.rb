ccf2d66e14cdf005a1cd3bf417e44962fff12c1c
thinca <thinca@gmail.com>
2011-05-21 02:42:06 +0900

    Added -p option to vitalize.rb.

--- vitalize.rb
+++ vitalize.rb
@@ -1,5 +1,6 @@
 require 'pathname'
 require 'fileutils'
+require 'optparse'
 
 def sh(o)
   puts o
@@ -10,15 +11,20 @@ def writefile(fname, content)
   File.open(fname, 'w') {|io| io.write content }
 end
 
+pluginname = nil
+opts = OptionParser.new
+opts.on("-p", "--plugin-name NAME"){|name| pluginname = name}
+opts.parse!(ARGV)
+
 if ARGV.size < 2
-  warn "usage: ruby vitalize.rb {vital.vim dir} {your project dir} [{sha1}]"
+  warn "usage: ruby vitalize.rb {vital.vim dir} {your project dir} [{sha1}] [-p pluginname]"
   warn "example: ruby vitalize.rb ~/git/vital.vim ~/.vim/bundle/unite.vim 1896f2"
   abort
 end
 
 vitaldir = File.expand_path ARGV.shift
 yourdir = File.expand_path ARGV.shift
-pluginname = Pathname(yourdir).basename.to_s.sub(/\.vim$/, '').gsub(/\W/, '_')
+pluginname ||= Pathname(yourdir).basename.to_s.sub(/\.vim$/, '').gsub(/\W/, '_')
 sha1 = ARGV.shift
 
 placeholders = lambda do |x|
