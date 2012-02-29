a3393b19f1a5f39cfbf5f9486ef222cc9cc6ac00
thinca <thinca@gmail.com>
2012-02-29 12:03:31 +0900

    Removed old vitalize.rb

--- update_vital.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VITAL_HOME=`dirname "$0"`
-TARGET=$1
-[ "x$TARGET" == "x" ] && TARGET=`pwd`
-[ "x$MSYSTEM" != "x" ] && [ "`which ruby | grep ^/`" != "" ] && TARGET=`echo "$TARGET" | sed 's/^\/\([a-zA-Z]\)/\1:/'`
-ruby "$VITAL_HOME/vitalize.rb" "$VITAL_HOME" "$TARGET"
