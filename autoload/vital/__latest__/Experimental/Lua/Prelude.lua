f048e8efe694e35d2901e5d6f00599fc8281f405
ujihisa <ujihisa@gmail.com>
2013-12-24 16:32:51 -0800

    Experimental.Lua.Prelude: initial prototype of vim/lua interaction for vital

---
+++ autoload/vital/__latest__/Experimental/Lua/Prelude.lua
@@ -0,0 +1,74 @@
+local public = {lua = {}, vim = {}}
+
+function public.lua.plus(x, y)
+  return x + y
+end
+
+public.vim.plus = public.lua.plus
+
+function public.lua.map(list, f)
+  print("lua.map", list, f)
+  local memo = {}
+  for _, v in ipairs(list) do
+    table.insert(memo, f(v))
+  end
+  return memo
+end
+
+local is_list = function(tbl)
+  -- http://stackoverflow.com/questions/6077006/how-can-i-check-if-a-lua-table-contains-only-sequential-numeric-indices
+  local numKeys = 0
+  for _, _ in pairs(tbl) do
+    numKeys = numKeys+1
+  end
+  local numIndices = 0
+  for _, _ in ipairs(tbl) do
+    numIndices = numIndices+1
+  end
+  return numKeys == numIndices
+end
+
+function public.lua.to_lua(vobj)
+  if vim.type(vobj) == 'list' then
+    local memo = {}
+    for v in vobj() do
+      table.insert(memo, v)
+    end
+    return memo
+  end
+end
+
+local lualist2vimlist = function(lualist)
+  local memo = vim.list()
+  for _, v in ipairs(lualist) do
+    memo:add(public.lua.from_lua(v))
+  end
+  return memo
+end
+
+local luadict2vimdict = function(luadict)
+  local memo = vim.dict()
+  for k, v in pairs(luadict) do
+    memo[k] = public.lua.from_lua(v)
+  end
+  return memo
+end
+
+function public.lua.from_lua(x)
+  if type(x) == "table" then
+    if is_list(x) then
+      return lualist2vimlist(x)
+    else
+      return luadict2vimdict(x)
+    end
+  else
+    return x
+  end
+end
+
+function public.vim.map(list, f)
+  print("vim.map", list, f)
+  return public.lua.from_lua(public.lua.map(public.lua.to_lua(list), f))
+end
+
+_G[vital_context] = public
---
+++ autoload/vital/__latest__/Experimental/Lua/Prelude.vim
@@ -0,0 +1,36 @@
+" Lua.Prelude
+" Author: Tatsuhiro Ujihisa
+
+let s:save_cpo = &cpo
+set cpo&vim
+
+let s:sfile = expand('<sfile>:p')
+
+function! s:_vital_loaded(V)
+  if has('lua')
+    execute printf('lua vital_context = "%s"', s:sfile)
+    call luaeval('dofile(_A)', substitute(s:sfile, '.vim$', '.lua', ''))
+  else
+    throw "Vital.Experimental.Lua.Prelude: You don't have if_lua."
+  endif
+endfunction
+
+function! s:plus(x, y)
+  return luaeval('_G[_A[0]].vim.plus(_A[1], _A[2])', [s:sfile, a:x, a:y])
+endfunction
+
+function! s:map(list, f)
+  return luaeval('_G[_A[0]].vim.map(_A[1], _A[2])', [s:sfile, a:list, a:f])
+endfunction
+
+" for testing
+function! s:mapinc(list)
+  return luaeval('_G[_A[0]].vim.map(_A[1], function(x) return x + 1 end)', [s:sfile, a:list])
+endfunction
+
+function! s:debug_sfile()
+  return s:sfile
+endfunction
+
+let &cpo = s:save_cpo
+unlet s:save_cpo
