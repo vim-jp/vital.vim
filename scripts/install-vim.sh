0f132d556c194146ec47adfb868eced12006bcef
lambdalisue <lambdalisue@hashnote.net>
2016-03-18 09:30:27 +0900

    Revert "Fix Vim's version to 7.4.1576"
    This reverts commit 4e1cc393450191a098f21c96e2a82eb2090d1b46.

--- scripts/install-vim.sh
+++ scripts/install-vim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 if [ x"$HEAD" = "xyes" ]; then
-  git clone --depth 1 --single-branch --branch v7.4.1576 https://github.com/vim/vim /tmp/vim
+  git clone --depth 1 https://github.com/vim/vim /tmp/vim
   cd /tmp/vim
   ./configure --prefix="$HOME/vim" --with-features=huge \
     --enable-perlinterp --enable-pythoninterp --enable-python3interp \
