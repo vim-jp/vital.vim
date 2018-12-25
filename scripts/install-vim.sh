acefb0b4de7963045be7a676eec20774c124b477
tyru <tyru.exe@gmail.com>
2018-12-25 11:12:39 +0900

    tmp

--- scripts/install-vim.sh
+++ scripts/install-vim.sh
@@ -16,6 +16,7 @@ case "${TRAVIS_OS_NAME}" in
 		;;
 	osx)
 		brew install macvim
+		brew uninstall ruby
 		# Instead of --with-override-system-vim, manually link the executable because
 		# it prevents MacVim installation with a bottle.
 		ln -sf "$(brew --prefix macvim)/bin/mvim" "/usr/local/bin/vim"
