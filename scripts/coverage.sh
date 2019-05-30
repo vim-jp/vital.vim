f35eb1dcea63bb0e07e46f6fc980ca8666dc9964
ichizok <gclient.gaap@gmail.com>
2019-05-31 01:38:32 +0900

    Fix covimerage in osx

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,6 +6,17 @@ if [[ "${THEMIS_PROFILE}" == "" ]]; then
     exit
 fi
 
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+    export PATH=$(
+    for dir in ${HOME}/Library/Python/*; do
+        if [[ -e "${dir}/bin/covimerage" ]]; then
+            echo -n "${dir}/bin:"
+            break
+        fi
+    done
+    )$PATH
+fi
+
 # Workaround: avoid covimerage error in Vim 8.1.0365 or later
 vim -u NONE -i NONE -N -e -s '+g/Defined:/d' +wq "${THEMIS_PROFILE}"
 
