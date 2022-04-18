c70c55fc8c1d00cec8f0eecbaad2ab3e36746e3c
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2022-04-18 20:08:39 +0900

    fix: cd fail to exit 1

--- .github/actions/vital-throw-message/entrypoint.sh
+++ .github/actions/vital-throw-message/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
-  cd "${GITHUB_WORKSPACE}" || exit
+  cd "${GITHUB_WORKSPACE}" || exit 1
   git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
 fi
 
